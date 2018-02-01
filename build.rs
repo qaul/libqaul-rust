857875f71265f27ef74bc79b81500de580bd9c46
Katharina Fey <kookie@spacekookie.de>
2018-02-01 02:28:05 +0100

    Submodule stuff

--- build.rs
+++ build.rs
@@ -3,28 +3,36 @@ extern crate cmake;
 
 use std::env;
 use std::path::PathBuf;
+use cmake::Config;
 
 fn main() {
 
-    let dst = cmake::build("qaul.net");
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=libqaul");
+    let mut dst = Config::new("qaul.net")
+                 .define("GUI", "CLI")
+                 .build();
+
+    dst.push("build");
+    dst.push("src");
+    dst.push("libqaul");
+
+    println!("cargo:rustc-link-search={}", dst.display());
+    println!("cargo:rustc-link-lib=libqaul");
 
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
-    // println!("cargo:rustc-link-lib=bz2");
+    // println!("cargo:rustc-link-lib=libqaul");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     // let bindings = bindgen::Builder::default()
-    //     // The input header we would like to generate
-    //     // bindings for.
-    //     .header("wrapper.h")
-    //     // Finish the builder and generate the bindings.
-    //     .generate()
-    //     // Unwrap the Result and panic on failure.
-    //     .expect("Unable to generate bindings");
+        // The input header we would like to generate
+        // bindings for.
+        // .header("qaul.net/")
+        // Finish the builder and generate the bindings.
+        // .generate()
+        // Unwrap the Result and panic on failure.
+        // .expect("Unable to generate bindings");
 
     // // Write the bindings to the $OUT_DIR/bindings.rs file.
     // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
